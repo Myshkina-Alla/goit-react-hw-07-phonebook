4dece2ca74bd6e27f6f427e3ee139c8af968b6af
Myshkina-Alla <myshkina.alla@icloud.com>
2024-01-28 21:09:17 +0200

    task is completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { App } from 'components';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter basename="/goit-react-hw-07-phonebook">
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
